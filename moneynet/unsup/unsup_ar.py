6504ec1dd1a25334a2758fda367a930f5479110b
j-pong <ljh93ljh@gmail.com>
2020-03-20 11:37:03 +0900

    report for plot one fig when scalar in same legend

--- moneynet/unsup/unsup_ar.py
+++ moneynet/unsup/unsup_ar.py
@@ -51,7 +51,7 @@ class Reporter(object):
         fig.savefig(os.path.join(sav_dir, filename))
         plt.close()
 
-    def report_plot_buffer(self, keys, epoch, tag=None):
+    def report_plot_buffer(self, keys, epoch, tag=None, merge=False):
         for key in keys:
             scalar = self.report_dict[key]
             if tag is not None:
@@ -65,19 +65,38 @@ class Reporter(object):
                 except:
                     self.report_buffer[key] = [scalar]
 
-        for key in keys:
+        if merge:
             fig = plt.figure()
             ax = fig.add_subplot(1, 1, 1)
             self.set_style(ax)
             fig.suptitle('epoch : {}'.format(epoch))
-            if tag is not None:
-                key = key + tag
-            ax.plot(self.report_buffer[key])
+            for i, key in enumerate(keys):
+                if tag is not None:
+                    key = key + tag
+                    if i == 0:
+                        filename = '{}.png'.format(key)
+                else:
+                    if i == 0:
+                        filename = '{}.png'.format(key)
+                ax.plot(self.report_buffer[key], label=key)
             ax.grid()
-
-            filename = '{}.png'.format(key)
+            ax.legend()
             fig.savefig(os.path.join(self.outdir, filename))
             plt.close()
+        else:
+            for key in keys:
+                fig = plt.figure()
+                ax = fig.add_subplot(1, 1, 1)
+                self.set_style(ax)
+                fig.suptitle('epoch : {}'.format(epoch))
+                if tag is not None:
+                    key = key + tag
+                ax.plot(self.report_buffer[key])
+                ax.grid()
+
+                filename = '{}.png'.format(key)
+                fig.savefig(os.path.join(self.outdir, filename))
+                plt.close()
 
     def add_report_attribute(self, attribute):
         for key in attribute.keys():
@@ -225,14 +244,14 @@ def train(args):
             filename = 'epoch{}_{}_attn.png'.format(epoch + 1, sample_name.split('.')[0])
             reporter.report_image(keys=['attn0', 'attn1', 'attn2', 'attn3', 'attn4'], filename=filename)
             filename = 'epoch{}_{}_xdis.png'.format(epoch + 1, sample_name.split('.')[0])
-            reporter.report_image(keys=['x_dis0', 'x_dis1', 'x_dis2', 'x_dis3', 'x_dis4'], filename=filename)
+            reporter.report_image(keys=['x_ele0', 'x_ele1', 'x_ele2', 'x_ele3', 'x_ele4'], filename=filename)
             filename = 'epoch{}_{}_xres.png'.format(epoch + 1, sample_name.split('.')[0])
             reporter.report_image(keys=['x_res0', 'x_res1', 'x_res2', 'x_res3', 'x_res4'], filename=filename)
             filename = 'epoch{}_{}.png'.format(epoch + 1, sample_name.split('.')[0])
             reporter.report_image(keys=['target', 'pred_y', 'pred_x', 'res_x'], filename=filename)
-            reporter.report_plot_buffer(keys=['loss', 'loss_x', 'loss_y'], epoch=epoch + 1, tag='_eval')
+            reporter.report_plot_buffer(keys=['loss', 'loss_x', 'loss_y'], epoch=epoch + 1, tag='_eval', merge=True)
         if (epoch + 1) % args.low_interval_epochs == 0:
-            reporter.report_plot_buffer(keys=['loss'], epoch=epoch + 1)
+            reporter.report_plot_buffer(keys=['loss', 'loss_x', 'loss_y'], epoch=epoch + 1, merge=True)
         if (epoch + 1) % args.save_interval_epochs == 0:
             filename = 'epoch{}.ckpt'.format(epoch + 1)
             torch.save(model.state_dict(), os.path.join(args.outdir, filename))
