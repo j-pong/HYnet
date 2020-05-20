7e8d647d6ff413c4bea5c7c905715fa88047de04
seas2nada <45331160+seas2nada@users.noreply.github.com>
2020-05-21 03:27:09 +0900

    cosine rampdown applied

--- moneynet/nets/pytorch_backend/ICT/nets_utils.py
+++ moneynet/nets/pytorch_backend/ICT/nets_utils.py
@@ -61,6 +61,11 @@ def sigmoid_rampup(current, rampup_length):
         phase = 1.0 - current / rampup_length
         return float(np.exp(-5.0 * phase * phase))
 
+def cosine_rampdown(current, rampdown_length):
+    """Cosine rampdown from https://arxiv.org/abs/1608.03983"""
+    assert 0 <= current <= rampdown_length
+    return float(.5 * (np.cos(np.pi * current / rampdown_length) + 1))
+
 def update_ema_variables(model, ema_model, alpha, global_step):
     # Use the true average until the exponential average is more correct
     alpha = min(1 - 1 / (global_step + 1), alpha)
