2d9f3a4c2c1c6fbba20a9abc396098c42b73e893
j-pong <ljh93ljh@gmail.com>
2020-07-08 17:34:05 +0900

    hidden space loss for discont.

--- moneynet/nets/pytorch_backend/ar_unsup_simnn.py
+++ moneynet/nets/pytorch_backend/ar_unsup_simnn.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import logging
-import six
 
 import numpy as np
 
@@ -70,17 +69,18 @@ class NetTransform(nn.Module):
 
         # related task
         self.brewing = args.brewing
-        self.field_var = args.field_var
-        space = np.linspace(0, self.odim - 1, self.odim)
-        self.field = np.expand_dims(
-            np.stack([gaussian_func(space, i, self.field_var) for i in range(self.odim)], axis=0), 0)
-        self.field = torch.from_numpy(self.field / np.amax(self.field))
+        # self.field_var = args.field_var
+        # space = np.linspace(0, self.odim - 1, self.odim)
+        # self.field = np.expand_dims(
+        #     np.stack([gaussian_func(space, i, self.field_var) for i in range(self.odim)], axis=0), 0)
+        # self.field = torch.from_numpy(2.0 - self.field / np.amax(self.field))
 
         # inference part with action and selection
         self.engine = Inference(idim=self.idim, odim=self.idim, args=args)
 
         # network training related
         self.criterion = SeqMultiMaskLoss(criterion=nn.MSELoss(reduction='none'))
+        self.criterion_h = nn.BCELoss(reduction='none')
 
         # reporter for monitoring
         self.reporter = Reporter()
@@ -95,9 +95,26 @@ class NetTransform(nn.Module):
         x = (x - m) / v
         return x
 
-    def calculate_energy(self, start_state, end_state, func, ratio, flows={'s': None,
-                                                                           't': None,
-                                                                           'e': None}):
+    @staticmethod
+    def fb(x):
+        xs = []
+        for i in range(x.size(-1)):
+            if i != 0:
+                x_f = torch.cumsum(x[:, :, i:], dim=-1)
+                x_b = torch.cumsum(x[:, :, :i].flip(dims=[-1]), dim=-1).flip(dims=[-1])
+                xs.append(torch.cat([x_b, x_f], dim=-1))
+            else:
+                xs.append(torch.cumsum(x[:, :, :], dim=-1))
+        xs = torch.stack(xs, dim=-1)  # B, tnum, T, T
+        norm = torch.max(xs)
+        if  norm == 0.0:
+            raise ValueError('Energy maximum value is {}'.format(torch.max(xs)))
+        else:
+            xs = 1.0 - xs / norm
+
+        return xs
+
+    def calculate_energy(self, start_state, end_state, func, ratio, flows={'e': None}):
         with torch.no_grad():
             # prepare data
             p_hat = self.engine.brew_(module_list=func, ratio=ratio)
@@ -116,21 +133,17 @@ class NetTransform(nn.Module):
             w_hat_x_p = torch.relu(w_hat_x)
             w_hat_x_n = torch.relu(-w_hat_x)
 
-            # calculate movement of each node : transition state for field
-            move_flow = (torch.abs(w_hat_x) * self.field.to(w_hat.device)).mean(-1).mean(-1).unsqueeze(-1)
             # relation with sign : how much state change in amplitude space
-            time_flow = (torch.abs(start_state) * w_hat_x_n.sum(-1)).mean(-1, keepdim=True) / \
-                        (torch.abs(start_state) * w_hat_x_p.sum(-1)).mean(-1, keepdim=True)
+            energy = (torch.abs(start_state) * w_hat_x_n.sum(-1)).mean(-1, keepdim=True) / \
+                     (torch.abs(start_state) * w_hat_x_p.sum(-1)).mean(-1, keepdim=True)
+            energy[torch.isnan(energy)] = 0.0
+            # energy[torch.isinf(energy)] = 0.0
+            energy = 1.0 - energy
 
             # calculate energy
-            seq_energy_mask = time_flow
             for key in flows.keys():
-                if key == 's':
-                    flows[key] = move_flow.view(bsz, tnsz, tsz)
-                elif key == 't':
-                    flows[key] = time_flow.view(bsz, tnsz, tsz)
-                elif key == 'e':
-                    flows[key] = seq_energy_mask.view(bsz, tnsz, tsz)
+                if key == 'e':
+                    flows[key] = torch.relu(energy).view(bsz, tnsz, tsz)
                 else:
                     raise AttributeError("'{}' type of augmentation factor is not defined!".format(key))
 
@@ -141,6 +154,7 @@ class NetTransform(nn.Module):
         xs_pad_in = xs_pad_in[:, :max(ilens)].unsqueeze(1)  # for data parallel
         xs_pad_out = xs_pad_out[:, :max(ilens)].transpose(1, 2)
         seq_mask = make_pad_mask((ilens).tolist()).to(xs_pad_in.device)
+        masks = [seq_mask.unsqueeze(1).repeat(1, self.tnum, 1).view(-1, 1)]
 
         xs_pad_in_m = torch.mean(xs_pad_in, dim=-1, keepdim=True)
         xs_pad_in_v = torch.mean(torch.pow(xs_pad_in - xs_pad_in_m, 2), dim=-1, keepdim=True)
@@ -152,28 +166,49 @@ class NetTransform(nn.Module):
                       'seq_energy': None,
                       'kernel': None}
 
-        masks = [seq_mask.unsqueeze(1).repeat(1, self.tnum, 1).view(-1, 1)]
-
         # embedding task
         h, ratio = self.engine(xs_pad_in, self.engine.encoder)  # B, 1, Tmax, idim
-        h = self.mvn(h)
-        h = h * xs_pad_in_v + xs_pad_in_m
-
         if self.brewing:
             flows = self.calculate_energy(start_state=xs_pad_in.contiguous(), end_state=h,
                                           func=self.engine.encoder,
                                           ratio=ratio,
                                           flows={'e': None})  # B, Tmax, 1
-            seq_energy_mask = flows['e']
+            energy = flows['e']
             if buffering:
-                self.buffs['seq_energy_enc'] = seq_energy_mask[:, :, :400]
+                self.buffs['seq_energy_enc'] = energy[:, :, :400]
+        h = self.mvn(h)
+        h = h * xs_pad_in_v + xs_pad_in_m
 
         # evaluate hidden space similarity
-        kernel = torch.matmul(h, h.transpose(-2, -1))  # B, T, T
+        kernel = torch.matmul(h, h.transpose(-2, -1))  # B, 1, T, T
 
         # long time prediction task
         h = h.repeat(1, self.tnum, 1, 1)
         xs_pad_out_hat, ratio = self.engine(h, self.engine.decoder)
+        if self.brewing:
+            flows = self.calculate_energy(start_state=h, end_state=xs_pad_out.contiguous(),
+                                          func=self.engine.decoder,
+                                          ratio=ratio,
+                                          flows={'e': None})  # B, Tmax, 1
+            energy = flows['e']
+            kernel_target = self.fb(energy)
+            if buffering:
+                self.buffs['seq_energy_dec'] = kernel_target[:, :, :400, :400]
+
+            # compute loss of hidden space
+            seq_mask_kernel = (1 - torch.matmul((~seq_mask).unsqueeze(-1).float(),
+                                                (~seq_mask).unsqueeze(1).float())).bool()
+            tsz = seq_mask_kernel.size(1)
+            loss_e = self.criterion_h(input=torch.sigmoid(kernel), target=kernel_target[:, 0:1].detach())
+            loss_e = loss_e.view(-1, tsz, tsz).masked_fill(seq_mask_kernel, 0).mean()
+            if torch.isnan(kernel_target.sum()):
+                print("target nan")
+            if torch.isnan(kernel.sum()):
+                print("output nan")
+            discontinuity = 0.0
+        else:
+            loss_e = 0.0
+            discontinuity = 0.0
         xs_pad_out_hat = self.mvn(xs_pad_out_hat)
         xs_pad_out_hat = xs_pad_out_hat * xs_pad_out_v + xs_pad_out_m
         if buffering:
@@ -181,28 +216,14 @@ class NetTransform(nn.Module):
             self.buffs['kernel'] = kernel
 
         # compute loss of total network
-        if self.brewing:
-            flows = self.calculate_energy(start_state=h, end_state=xs_pad_out.contiguous(),
-                                          func=self.engine.decoder,
-                                          ratio=ratio,
-                                          flows={'e': None})  # B, Tmax, 1
-            seq_energy_mask = flows['e']
-            if buffering:
-                self.buffs['seq_energy_dec'] = seq_energy_mask[:, :, :400]
-
-            loss_e = seq_energy_mask.view(-1, 1).masked_fill(masks[0], 0).mean()
-            discontinuity = 0.0
-        else:
-            loss_e = 0.0
-            discontinuity = 0.0
-
-        loss = self.criterion(xs_pad_out_hat.view(-1, self.idim),
-                              xs_pad_out.contiguous().view(-1, self.idim),
-                              masks).mean()
-        if not torch.isnan(loss) or not torch.isnan(loss_e):
-
+        loss_g = self.criterion(xs_pad_out_hat.view(-1, self.idim),
+                                xs_pad_out.contiguous().view(-1, self.idim),
+                                masks).mean()
+        loss = loss_g + loss_e
+        if not torch.isnan(loss):
             self.reporter.report({'loss': float(loss),
                                   'e_loss': float(loss_e),
+                                  'g_loss': float(loss_g),
                                   'discontinuity': float(discontinuity)})
         else:
             print("loss (=%f) is not c\orrect", float(loss))
