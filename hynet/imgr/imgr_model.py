7ee3faca52cdf4aa9b552da643034cbf81268a9f
j-pong <ljh93ljh@gmail.com>
2020-09-19 11:30:09 +0900

    BREW for cnn for superposition problem solving

--- hynet/imgr/imgr_model.py
+++ hynet/imgr/imgr_model.py
@@ -12,7 +12,7 @@ from typeguard import check_argument_types
 from espnet2.torch_utils.device_funcs import force_gatherable
 from espnet2.train.abs_espnet_model import AbsESPnetModel
 
-from hynet.imgr.brew_layer import BrewLayer, BrewAttLayer
+from hynet.layers.brew_layer import BrewLayer, BrewCnnLayer, BrewAttLayer
 
 class HynetImgrModel(AbsESPnetModel):
     """Image recognition model"""
@@ -21,51 +21,68 @@ class HynetImgrModel(AbsESPnetModel):
         assert check_argument_types()
         super().__init__()
 
-        self.max_iter = 1
+        self.max_iter = 2
 
-        self.hidden_size = 64
-        self.wh = 22
+        self.cnn_hidden_sizes = [32, 64]
+        self.cnn_kernel_sizes = [3, 3]
+        self.wh = 24
 
+        self.proj_hidden_size = 128
         self.bias = True
 
-        self.brew_cnn_layer = BrewAttLayer(
-            hidden_size=self.hidden_size)
+        self.brew_cnn_layer = BrewCnnLayer(
+            kernel_size=self.cnn_kernel_sizes[0],
+            hidden_size=self.cnn_hidden_sizes[0],
+            in_channel=1,
+            wh=26)
+        self.brew_cnn_layer2 = BrewCnnLayer(
+            kernel_size=self.cnn_kernel_sizes[1],
+            hidden_size=self.cnn_hidden_sizes[1], 
+            in_channel=self.cnn_hidden_sizes[0],
+            wh=self.wh)
+
+        self.flatten = nn.Flatten()
 
         self.brew_layer = BrewLayer(
-            sample_size=self.wh * self.wh* self.hidden_size,
-            hidden_size=self.hidden_size,
+            sample_size=self.wh * self.wh * self.cnn_hidden_sizes[1],
+            hidden_size=self.proj_hidden_size,
             target_size=10,
             bias=self.bias)
+
+        self.csim = nn.CosineSimilarity(dim=2)
 
         self.criterion = nn.CrossEntropyLoss()
         self.criterion_mse = nn.MSELoss()
 
     def minimaxn(
         self, 
-        x: torch.Tensor
+        x: torch.Tensor,
+        dim: int
     ):
-        max_x = torch.max(x, dim=-1, keepdim=True)[0].detach()
-        min_x = torch.min(x, dim=-1, keepdim=True)[0].detach()
+        max_x = torch.max(x, dim=dim, keepdim=True)[0]
+        min_x = torch.min(x, dim=dim, keepdim=True)[0]
         x = (x - min_x) / (max_x - min_x)
 
         return x
 
     def inv(
         self,
+        x,
         label_hat: torch.Tensor,
-        p_hat: [torch.Tensor, torch.Tensor]
+        w_hat: torch.Tensor
     ):
-        w, b = p_hat
+        assert len(label_hat.size()) == 3
 
-        label_hat = label_hat - b
-        label_hat = torch.softmax(label_hat, dim=-1)
+        sign = torch.sign(x).unsqueeze(-1)
+        sign[sign==0.0] = 1.0
+        w_hat *= sign
 
-        w_pos = torch.relu(w)
-        attn_pos = torch.matmul(label_hat.unsqueeze(-2), w_pos.transpose(-2, -1))
+        w_pos = torch.relu(w_hat)
+        attn_pos = torch.matmul(label_hat, w_pos.transpose(-2, -1))
         attn_pos = attn_pos.squeeze(-2)
 
-        w_neg = torch.relu(-1.0 * w)
-        attn_neg = torch.matmul(label_hat.unsqueeze(-2), w_neg.transpose(-2, -1))
+        w_neg = torch.relu(-1.0 * w_hat)
+        attn_neg = torch.matmul(label_hat, w_neg.transpose(-2, -1))
         attn_neg = attn_neg.squeeze(-2)
 
         return attn_pos, attn_neg
@@ -78,52 +95,89 @@ class HynetImgrModel(AbsESPnetModel):
         batch_size = image.shape[0]
         image = image.view(batch_size, -1).float()
 
-        logger = {'imgs':[[],[]], 
+        logger = {'imgs':[], 
                   'attns': [[],[],[]], 
-                  'accs':[], 
+                  'accs':[],
+                  'accs_cs': None, 
                   'loss_brew': None}
 
         losses = []
         for i in range(self.max_iter):
-            # 0. normalization
-            image = self.minimaxn(image).detach()
+            # 0. prepare data
+            if i == 0:
+                image = self.minimaxn(image, dim=1).detach()
+                image = image.view(batch_size, 1, 28, 28)
 
-            # 1. feedforward neural network 
+            # 1. feedforward neural network
             feat, _ = self.brew_cnn_layer(image)
-            feat_flat = torch.flatten(feat, start_dim=1)
+            feat, _ = self.brew_cnn_layer2(feat)
+            if i > 0:
+                attn_pos = attn_pos.flatten(start_dim=2)
+                attn_pos = self.minimaxn(attn_pos, dim=2)
+                attn_pos = attn_pos.view(batch_size, -1, self.wh, self.wh)
+                feat = feat * (1 - attn_pos.detach())
+            feat_flat = self.flatten(feat)
             label_hat, ratio = self.brew_layer(feat_flat)
 
             # 2. brewing and check loss of p_hat results and normal result
             p_hat = self.brew_layer.brew(ratio=ratio)
+            
             # 2.1 check brewing error
             if i == 0:
                 label_hat_hat = torch.matmul(feat_flat.unsqueeze(-2), p_hat[0])
                 label_hat_hat = label_hat_hat.squeeze(-2)
                 if self.bias:
                     label_hat_hat += p_hat[1]
-                logger['loss_brew'] = torch.pow(label_hat - label_hat_hat, 2).mean() 
-
+                logger['loss_brew'] = torch.pow(label_hat - label_hat_hat, 2).mean()
+                
             # 3. caculate measurment 
-            loss = self.criterion(label_hat, label)
-            losses.append(loss)
+            losses.append(self.criterion(label_hat, label))
             # 3.1 other measurment
             acc = self._calc_acc(label_hat, label)        
             logger['accs'].append(acc)
 
             # 4. inverse atte ntion with feature
-            attn_pos, attn_neg = self.inv(label_hat, p_hat)
+            inverse_application = 'sup'
+            if inverse_application == 'sup':
+                label_hat = label_hat - p_hat[1]
+                label_hat = torch.softmax(label_hat, dim=-1).unsqueeze(-2)
+                
+                attn_pos, attn_neg = self.inv(feat_flat, label_hat, p_hat[0])
 
+                attn_pos = attn_pos.view(batch_size, self.cnn_hidden_sizes[1], self.wh, self.wh)
+                attn_neg = attn_neg.view(batch_size, self.cnn_hidden_sizes[1], self.wh, self.wh)
+
+                logger['accs_cs'] = 0.0
+
+            elif inverse_application == 'sim':
+                label_hat = F.one_hot(torch.arange(start=0, end=10), 
+                                      num_classes=10).float().to(label.device)
+                label_hat = label_hat.view(1, 10, 10)
+                label_hat = label_hat.repeat(batch_size, 1, 1)
+
+                attn_pos, attn_neg = self.inv(feat_flat, label_hat, p_hat[0])
+                label_hat_hat_cs = self.csim(attn_pos, feat.flatten(start_dim=1).unsqueeze(1))
+
+                logger['accs_cs'] = self._calc_acc(label_hat_hat_cs, label)     
+
+                
             # 5. for logging
-            logger['attns'][0].append(attn_pos[0].view(self.hidden_size, self.wh, self.wh).mean(-3))
-            logger['attns'][1].append(attn_neg[0].view(self.hidden_size, self.wh, self.wh).mean(-3))
+            if not self.training:
+                if inverse_application == 'sim':
+                    attn_pos = attn_pos[:, 0]
+                    attn_neg = attn_neg[:, 0]
 
-            logger['imgs'][0].append(image[0].view(28, 28))
-            logger['imgs'][1].append(feat[0].mean(-3))
+                logger['attns'][0].append(attn_pos[0].mean(-3))
+                logger['attns'][0].append(attn_pos[0, 0])
+                logger['attns'][0].append(attn_pos[0, 1])
 
-            kernel = self.brew_cnn_layer.atts[0].kernel
-            logger['attns'][2].append(kernel[0])
-            kernel = self.brew_cnn_layer.atts[1].kernel
-            logger['attns'][2].append(kernel[0])
+                logger['attns'][1].append(attn_neg[0].mean(-3))
+                logger['attns'][1].append(attn_neg[0, 0])
+                logger['attns'][1].append(attn_neg[0, 1])
+
+                logger['imgs'].append(feat[0].mean(-3))
+                logger['imgs'].append(feat[0, 0])
+                logger['imgs'].append(feat[0, 1])
 
         loss = 0.0
         for los in losses:
@@ -133,12 +187,13 @@ class HynetImgrModel(AbsESPnetModel):
                 loss=loss.detach(),
                 loss_brew=logger['loss_brew'].detach(),
                 acc_start=logger['accs'][0],
-                acc_end=logger['accs'][-1]
+                acc_end=logger['accs'][-1],
+                acc_cs=logger['accs_cs'] 
             )
         if not self.training:
-            stats['aux'] = [logger['imgs'][0] + logger['imgs'][1], 
-                            logger['attns'][0] + logger['attns'][1], 
-                            logger['attns'][2]]
+            stats['aux'] = [logger['imgs'], 
+                            logger['attns'][0],
+                            logger['attns'][1]]
 
         loss, stats, weight = force_gatherable(
             (loss, stats, batch_size), loss.device)
---
+++ hynet/layers/brew_layer.py
@@ -0,0 +1,318 @@
+import numpy as np
+
+import torch
+import torch.nn.functional as F
+from torch import nn
+
+class BrewLayer(nn.Module):
+    def __init__(
+        self, 
+        sample_size,
+        hidden_size, 
+        target_size, 
+        bias=True):
+        super().__init__()
+
+        self.fnn = nn.ModuleList([
+            nn.Linear(sample_size, hidden_size, bias=bias),
+            nn.ReLU(),
+            nn.Linear(hidden_size, hidden_size, bias=bias),
+            nn.ReLU(),
+            nn.Linear(hidden_size, target_size, bias=bias)
+        ])
+
+    @torch.no_grad()
+    def calculate_ratio(self, x, x_base):
+        rat = x / x_base
+        sign = torch.sign(x_base)
+        rat *= sign
+        rat[x_base==0] = 0
+
+        return rat
+
+    @torch.no_grad()
+    def brew(self, ratio, split_dim=None, w_hat=None, b_hat=None):
+        for module in self.fnn:
+            if isinstance(module, nn.Linear):
+                w = (module.weight).transpose(-2,- 1)
+                b = module.bias
+
+                if w_hat is not None:
+                    w_hat = torch.matmul(w_hat, w)
+                    # (?, d, C) x (C, C*)  -> (?, d, C*)
+                if b is not None:
+                    if b_hat is not None:
+                        b_hat = torch.matmul(b_hat, w)
+                        # (?, C) x (C, C*) -> (?, C*)
+                        b_hat = b.unsqueeze(0) + b_hat
+            elif isinstance(module, nn.ReLU) or isinstance(module, nn.PReLU) or isinstance(module, nn.Tanh):
+                rat = ratio.pop(0)
+                rat = rat.view(-1, rat.size(-1))  # (?, C)
+
+                if w_hat is None:
+                    w_hat = rat.unsqueeze(1) * w.unsqueeze(0)
+                    # (?, 1, C1) * (1, d, C1)  -> (?, d, C1)
+                else:
+                    w_hat = rat.unsqueeze(1) * w_hat  # (?, 1, C*) * (?, d, C*)
+
+                if b is not None:
+                    if b_hat is None:
+                        b_hat = rat * b.unsqueeze(0)
+                        # (?, C1) * (1, C1) -> (?, C1)
+                    else:
+                        b_hat = rat * b_hat
+                        # (?, C) + (?, C) -> (?, C*)
+            else:
+                raise AttributeError(
+                    "Current network architecture, {}, is not supported!".format(module))
+
+        return w_hat, b_hat
+
+    def forward(self, x):
+        ratio = []
+        for idx, module in enumerate(self.fnn):
+            if isinstance(module, nn.Linear):
+                x = module(x)
+            elif isinstance(module, nn.ReLU) or isinstance(module, nn.PReLU) or isinstance(module, nn.Tanh):
+                x_base = x
+                x = module(x)
+                ratio.append(self.calculate_ratio(x, x_base))
+            else:
+                raise AttributeError(
+                    "Current network architecture is not supported!")
+
+        return x, ratio
+
+class AttLayer(nn.Module):
+    def __init__(
+        self):
+        super().__init__()
+
+        self.min_value = float(
+            np.finfo(torch.tensor(0, dtype=torch.float).numpy().dtype).min
+        )
+        self.mask_diag = False
+
+        self.softmax = nn.Softmax(dim=-1)
+
+        self.kernel = None
+
+    def forward(self, x):
+        _, tsz, csz = x.size()
+        q, k, v = torch.split(x, dim=-1, split_size_or_sections=int(csz / 3))
+
+        score = torch.matmul(k, q.transpose(-2, -1)) / np.sqrt(q.size(-1))
+        if self.mask_diag:
+            mask = torch.zeros_like(score)
+            mask[:, range(tsz), range(tsz)] = 1
+            mask = mask.bool()
+            score.masked_fill_(mask, self.min_value)
+
+        kernel = self.softmax(score)
+        # Todo(j-pong): Fix for gradient pass
+        # if self.mask_diag:
+        #     kernel.masked_fill_(mask, 0.0)
+        #     assert torch.sum((kernel.diagonal(dim1=-2, dim2=-1) != 0.0).float()) == 0.0
+        self.kernel = kernel
+
+        x = torch.matmul(kernel, v)
+
+        return x
+
+class BrewAttLayer(nn.Module):
+    def __init__(
+        self,
+        kernel_size,
+        hidden_size
+    ):
+        super().__init__()
+
+        self.hidden_size = hidden_size
+
+        self.atts = [AttLayer(), AttLayer()]
+
+        self.cnn = nn.ModuleList([
+            nn.Unfold(kernel_size=(kernel_size, kernel_size), stride=(1,1)),
+            nn.Linear(in_features=kernel_size * kernel_size * 1, 
+                      out_features=self.hidden_size * 3,
+                      bias=False),
+            self.atts[0],
+            nn.ReLU(),
+            nn.Linear(in_features=self.hidden_size,
+                      out_features=self.hidden_size * 3,
+                      bias=False),
+            self.atts[1],
+            nn.ReLU(),
+            nn.Fold(kernel_size=(1,1), output_size=(26, 26))
+        ])
+
+    def calculate_ratio(self, x, x_base): 
+        rat = x / x_base
+        rat[torch.isnan(rat) | torch.isinf(rat)] = 0.0
+
+        return rat
+    
+    def mv_nrom(self, x):
+        mean = torch.mean(x, dim=2, keepdim=True)
+        var = torch.mean(torch.pow(x - mean, exponent=2), dim=2, keepdim=True)
+        x = (x - mean) / var
+        return x
+
+    def brew(self, ratio, w_hat=None, b_hat=None, kernel=None):
+        for module in self.cnn:
+            if isinstance(module, nn.Unfold):
+                pass
+            elif isinstance(module, nn.Linear):
+                w = (module.weight).transpose(-2,- 1)
+                _, _, w = torch.split(w, 
+                                      dim=-1, 
+                                      split_size_or_sections=int(w.size(-1) / 3))
+                # (C, C*)
+                assert module.bias is None
+                if w_hat is not None:
+                    w_hat = torch.matmul(w_hat, w)
+                    # (B * w * h, d, C) x (C, C*)  -> (B * w * h, d, C*)
+            elif isinstance(module, nn.ReLU) or isinstance(module, nn.PReLU):
+                rat = ratio.pop(0)
+                rat = rat.transpose(-2, -1)
+                # (B, w * h, C*)
+                if w_hat is None:
+                    w_hat = rat.unsqueeze(2) * w.unsqueeze(0).unsqueeze(0)
+                    # (B, w * h, 1, C1) * (1, 1, d, C1) -> (B, w * h, d, C1)
+                else:
+                    w_hat = rat.unsqueeze(2) * w_hat
+                    # (B, w * h, 1, C*) * (B, w * h, d, C*) -> (B, w * h, d, C*) 
+                if kernel is not None:
+                    bsz, whsz, dsz, csz = w_hat.size()
+                    w_hat = torch.matmul(kernel, w_hat.flatten(start_dim=2))
+                    # (B, w * h, w * h) x (B, w * h, d * C*) -> (B, w * h, d * C*)
+                    w_hat = w_hat.view(bsz, whsz, dsz, csz)
+                    # (B, w * h, d, C*)
+            elif isinstance(module, AttLayer):
+                kerenl = ratio.pop(0)
+                # (B, w * h, w * h)
+            elif isinstance(module, nn.Fold):
+                pass
+            else:
+                raise AttributeError(
+                    "Current network architecture, {}, is not supported!".format(module))
+        return w_hat, b_hat
+
+    def forward(self, x):
+        ratio = []
+
+        batch_size = x.size(0)
+
+        x = x.view(batch_size, 1, 28, 28)
+
+        # make patch for weight        
+        for module in self.cnn:
+            if isinstance(module, nn.Unfold):
+                x = module(x)
+            elif isinstance(module, nn.Linear):
+                x = module(x.transpose(1, 2))
+                # (B, T, C)
+            elif isinstance(module, nn.ReLU) or isinstance(module, nn.PReLU):
+                x_base = x
+                x = module(x)
+                ratio.append(self.calculate_ratio(x, x_base))
+            elif isinstance(module, AttLayer):
+                mean = torch.mean(x, dim=2, keepdim=True)
+                var = torch.mean(torch.pow(x - mean, exponent=2), dim=2, keepdim=True)
+                # (B, T, C)
+
+                x = module(x)
+                x = self.mv_nrom(x)
+                x = x * var + mean 
+
+                x = x.transpose(1, 2)
+                # (B, C, T)
+                ratio.append(module.kernel)
+            elif isinstance(module, nn.Fold):
+                x = module(x)
+            else:
+                raise AttributeError(
+                    "Current network architecture, {}, is not supported!".format(module))
+
+        return x, ratio
+
+class BrewCnnLayer(nn.Module):
+    def __init__(
+        self,
+        kernel_size,
+        hidden_size,
+        in_channel,
+        wh
+    ):
+        super().__init__()
+
+        self.hidden_size = hidden_size
+
+        self.layers = nn.ModuleList([
+            nn.Unfold(kernel_size=(kernel_size, kernel_size), stride=(1,1)),
+            nn.Linear(in_features=kernel_size * kernel_size * in_channel, 
+                      out_features=self.hidden_size,
+                      bias=False),
+            nn.ReLU(),
+            nn.Fold(kernel_size=(1,1), output_size=(wh, wh))
+        ])
+
+    @torch.no_grad()
+    def calculate_ratio(self, x, x_base): 
+        rat = x / x_base
+        rat[torch.isnan(rat) | torch.isinf(rat)] = 0.0
+
+        return rat
+
+    @torch.no_grad()
+    def brew(self, ratio, w_hat=None, b_hat=None, kernel=None):
+        for layer in self.layers:
+            if isinstance(layer, nn.Unfold):
+                pass
+            elif isinstance(layer, nn.Linear):
+                w = (layer.weight).transpose(-2,- 1)
+                # (C, C*)
+                assert layer.bias is None
+                if w_hat is not None:
+                    w_hat = torch.matmul(w_hat, w)
+                    # (B * w * h, d, C) x (C, C*)  -> (B * w * h, d, C*)
+            elif isinstance(layer, nn.ReLU) or isinstance(layer, nn.PReLU):
+                rat = ratio.pop(0)
+                rat = rat.transpose(-2, -1)
+                # (B, w * h, C*)
+                if w_hat is None:
+                    w_hat = rat.unsqueeze(2) * w.unsqueeze(0).unsqueeze(0)
+                    # (B, w * h, 1, C1) * (1, 1, d, C1) -> (B, w * h, d, C1)
+                else:
+                    w_hat = rat.unsqueeze(2) * w_hat
+                    # (B, w * h, 1, C*) * (B, w * h, d, C*) -> (B, w * h, d, C*) 
+            elif isinstance(layer, nn.Fold):
+                pass
+            else:
+                raise AttributeError(
+                    "Current network architecture, {}, is not supported!".format(layer))
+        return w_hat, b_hat
+
+    def forward(self, x):
+        ratio = []
+
+        # make patch for weight        
+        for layer in self.layers:
+            if isinstance(layer, nn.Unfold):
+                x = layer(x)
+            elif isinstance(layer, nn.Linear):
+                x = layer(x.transpose(1, 2))
+                # (B, T, C)
+                x = x.transpose(1, 2)
+                # (B, C, T)
+            elif isinstance(layer, nn.ReLU) or isinstance(layer, nn.PReLU):
+                x_base = x
+                x = layer(x)
+                ratio.append(self.calculate_ratio(x, x_base))
+            elif isinstance(layer, nn.Fold):
+                x = layer(x)
+            else:
+                raise AttributeError(
+                    "Current network architecture, {}, is not supported!".format(layer))
+
+        return x, ratio
