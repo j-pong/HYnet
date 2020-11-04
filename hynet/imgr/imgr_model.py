precision error fix

hynet/imgr/imgr_model.py
@@ -124,8 +124,15 @@ class HynetImgrModel(AbsESPnetModel):
                 parm_norm = 0.0
                 for name, param in self.model.named_parameters():
                     if param.requires_grad:
-                        parm_norm += param.data.norm()  
+                        parm_abs = param.data.abs()
+                        m = parm_abs.mean()
+                        var = parm_abs.var()
+                        parm_norm += var
+                        # print(name, m, var)
+
+                # classification ce-loss
                 losses.append(self.criterion(logit, label))
+            # accuracy for classification
             acc = self._calc_acc(logit, label)        
             logger['accs'].append(acc)
 
@@ -139,8 +146,10 @@ class HynetImgrModel(AbsESPnetModel):
                         logit_softmax = torch.softmax(logit, dim=-1)
                         mask = F.one_hot(label, num_classes=self.out_ch).bool()
                         
-                        logit_softmax_cent = logit_softmax.masked_fill(~mask, 0.0)
-                        attn_cent = self.model.backward_linear(image, logit_softmax_cent)
+                        # logit_softmax_cent = logit_softmax.masked_fill(~mask, 0.0)
+                        logit_softmax_cent = mask.float()
+                        outputs = logit.masked_select(mask).unsqueeze(-1)
+                        attn_cent = self.model.backward_linear(image, logit_softmax_cent, outputs)
 
                         logit_softmax_other = logit_softmax.masked_fill(mask, 0.0)
                         attn_other = self.model.backward_linear(image, logit_softmax_other)
@@ -154,20 +163,21 @@ class HynetImgrModel(AbsESPnetModel):
                         attn = grads.squeeze()
                     elif self.xai_mode == 'ig':
                         ig = IntegratedGradients(self.model)
-                        attr_ig = attribute_image_features(self.model, label, ig, image, 
-                                                           baselines=image * 0)
+                        attr_ig, delta = attribute_image_features(self.model, label, ig, image, 
+                                                                  baselines=image * 0, 
+                                                                  return_convergence_delta=True)
                         attn = attr_ig.squeeze()
                         attn_cent = attn
                         attn_other = attn
 
-                        loss_brew = 0.0
+                        loss_brew = delta
                     elif self.xai_mode == 'ig_nt':
                         ig = IntegratedGradients(self.model)
                         nt = NoiseTunnel(ig)
                         attr_ig_nt = attribute_image_features(self.model, label, nt, image, 
-                                                                baselines=image * 0, 
-                                                                nt_type='smoothgrad',
-                                                                n_samples=4, stdevs=0.02)
+                                                              baselines=image * 0, 
+                                                              nt_type='smoothgrad',
+                                                              n_samples=4, stdevs=0.02)
                         attn = attr_ig_nt.squeeze(0)
                     elif self.xai_mode == 'dl':
                         dl = DeepLift(self.model)
@@ -187,10 +197,8 @@ class HynetImgrModel(AbsESPnetModel):
                     elif self.xai_mode == 'bg':
                         bg = BrewGradient(self.model)
                         attr_bg = bg.attribute(image, 
-                                               target=label,
-                                               baselines=image * 0)
+                                               target=label)
                         attn = attr_bg.squeeze()
-
                         attn_cent = attn
                         attn_other = attn
 

hynet/layers/brew.py
@@ -233,11 +233,18 @@ class BrewModel(BrewModule):
                 c_hat_cum = None
         return x
 
-    def backward_linear(self, x, y):
+    def backward_linear(self, x, y, outputs=None):
         with torch.no_grad():
             # backward
             attn = self.backward_linear_impl(y, self.decoder)
             attn = self.backward_linear_impl(attn, self.encoder)
+
+            if outputs is not None:
+                outputs_hat = (x * attn).flatten(start_dim=1).sum(-1, keepdim=True)
+                loss_brew = F.mse_loss(outputs, outputs_hat)
+                self.loss_brew = loss_brew
+            else:
+                pass
 
             # sign-field
             sign = torch.sign(x)
@@ -264,14 +271,14 @@ class BrewModel(BrewModule):
             self.attn_size = x_non_linear.size()
             x_non_linear = self.forward_impl(x_non_linear, self.decoder)
 
-            with torch.no_grad():
-                # linearization
-                x_linear = self.forward_linear_impl(x, self.encoder)
-                x_linear = self.forward_linear_impl(x_linear, self.decoder)
+            # with torch.no_grad():
+            #     # linearization
+            #     x_linear = self.forward_linear_impl(x, self.encoder)
+            #     x_linear = self.forward_linear_impl(x_linear, self.decoder)
 
-                # linearization error check
-                self.loss_brew = F.mse_loss(x_non_linear, x_linear).detach()
-                if self.loss_brew.float() > 1e-18:
-                    raise ValueError("loss of brew {} bigger than 1e-18".format(self.loss_brew))
+            #     # linearization error check
+            #     self.loss_brew = F.mse_loss(x_non_linear, x_linear).detach()
+            #     if self.loss_brew.float() > 1e-18:
+            #         raise ValueError("loss of brew {} bigger than 1e-18".format(self.loss_brew))
 
         return x_non_linear

hynet/layers/ig.py
@@ -6,7 +6,8 @@ import torch
 from torch import Tensor
 import torch.nn.functional as F
 
-from captum.attr._utils.common import _run_forward, _format_input
+from captum.attr._utils.common import _run_forward, _format_input, _select_targets
+from captum.attr._utils.gradient import apply_gradient_requirements, undo_gradient_requirements
 
 from captum.attr._utils.approximation_methods import approximation_parameters
 from captum.attr._utils.attribution import GradientAttribution
@@ -38,28 +39,60 @@ class BrewGradient(GradientAttribution):
         """
         GradientAttribution.__init__(self, forward_func)
         self.loss_brew = 0.0
+
     def attribute(  # type: ignore
         self,
         inputs: TensorOrTupleOfTensorsGeneric,
-        baselines: BaselineType = None,
         target: TargetType = None,
         return_convergence_delta: bool = False,
+        precision: str = 'float32'
     ) -> Union[
         TensorOrTupleOfTensorsGeneric, Tuple[TensorOrTupleOfTensorsGeneric, Tensor]
     ]:  
+        if precision == 'float32':
+            inputs = inputs.float()
+            self.forward_func.float()
+        elif precision == 'float64':
+            inputs = inputs.double()
+            self.forward_func.double()
+        else:
+            raise AttributeError()
+        inputs = _format_input(inputs)
+        gradient_mask = apply_gradient_requirements(inputs)
+
         with torch.autograd.set_grad_enabled(True):
-            inputs = inputs.requires_grad_()
             # runs forward pass
             outputs = _run_forward(self.forward_func, inputs, target)
             assert outputs[0].numel() == 1, (
                 "Target not provided when necessary, cannot"
                 " take gradient with respect to multiple outputs."
             )
-            grads = torch.autograd.grad(torch.unbind(outputs), inputs)
-        grads = grads[0].detach()
-        outputs_hat = (inputs * grads).flatten(start_dim=1).sum(-1, keepdim=True)
-        self.loss_brew = F.mse_loss(outputs_hat, outputs).detach()
+            
+            # calculate gradient
+            grads = torch.autograd.grad(torch.unbind(outputs), inputs)[0]
 
+        # clear gradient and detach from graph
+        undo_gradient_requirements(inputs, gradient_mask)
+
+        # back to the original precision
+        if precision == 'float32':
+            pass
+        elif precision == 'float64':
+            self.forward_func.float()
+        else:
+            raise AttributeError()
+        
+        # output type check
+        if isinstance(inputs, tuple):
+            inputs = inputs[0]
+
+        # check numerical error (why this error is not zero?)
+        # suspect shattered gradient or precision error
+        outputs_hat = (inputs * grads).flatten(start_dim=1).sum(-1, keepdim=True)
+        loss_brew = F.mse_loss(outputs, outputs_hat)
+        self.loss_brew = loss_brew
+
+        # importance of feature has sign field 
         sign = torch.sign(inputs)
         grads = grads * sign
 