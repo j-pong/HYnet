3ee8e180685702ed6a1aee348618e9760cf96156
seas2nada <dooh1474@naver.com>
2020-11-11 18:51:53 +0900

    modified

--- tools/install_demo_dependencies.sh
+++ tools/install_demo_dependencies.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo apt-get install -y python3-pyqt5 python-pyqt5.qtsql qttools5-dev-tools python3-pyaudio
-pip3 install scipy sounddevice matplotlib subprocess
+sudo apt-get install -y python3-pip python3-pyqt5 python-pyqt5.qtsql qttools5-dev-tools python3-pyaudio
+pip3 install scipy sounddevice matplotlib
