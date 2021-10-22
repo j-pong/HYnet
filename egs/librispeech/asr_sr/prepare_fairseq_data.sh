ce85c3a5a1616c04d209048ef0b679ef04d82697
seas2nada <dooh1474@naver.com>
2021-10-22 18:58:05 +0900

    add commands for train_960

--- egs/librispeech/asr_sr/prepare_fairseq_data.sh
+++ egs/librispeech/asr_sr/prepare_fairseq_data.sh
@@ -14,11 +14,18 @@ save_l_data_dir=$PWD/data/fairseq/train_clean_100/
 . ./path.sh || exit 1;
 . utils/parse_options.sh || exit 1;
 
+# generate train files
 python $PWD/../../../tools/fairseq/examples/wav2vec/wav2vec_manifest.py $ul_data_path --dest $save_ul_data_dir --ext flac --valid 0.01
 python $PWD/../../../tools/fairseq/examples/wav2vec/wav2vec_manifest.py $l_data_path --dest $save_l_data_dir --ext flac --valid 0
 
 split=train
+# get dictionary list file
 wget https://dl.fbaipublicfiles.com/fairseq/wav2vec/dict.ltr.txt -P $save_l_data_dir
+cp -r $save_l_data_dir/dict.ltr.txt $save_ul_data_dir/
+
+# make labels for train data
+python $PWD/../../../tools/fairseq/examples/wav2vec/libri_labels.py $save_l_data_dir/train.tsv --output-dir $save_l_data_dir --output-name $split
+python $PWD/../../../tools/fairseq/examples/wav2vec/libri_labels.py $save_ul_data_dir/train.tsv --output-dir $save_ul_data_dir --output-name $split
 
 # Prepare all dev/test dataset
 for dset in test_clean test_other dev_clean dev_other; do
@@ -27,8 +34,8 @@ for dset in test_clean test_other dev_clean dev_other; do
     python $PWD/../../../tools/fairseq/examples/wav2vec/wav2vec_manifest.py $test_data_dir --dest $save_test_data_dir --ext flac --valid 0
 
     cp -r $save_l_data_dir/dict.ltr.txt $save_test_data_dir/
-    python $PWD/../../../tools/fairseq/examples/wav2vec/libri_labels.py $save_l_data_dir/train.tsv --output-dir $save_l_data_dir --output-name $split
     python $PWD/../../../tools/fairseq/examples/wav2vec/libri_labels.py $save_test_data_dir/train.tsv --output-dir $save_test_data_dir --output-name $dset
     mv $save_test_data_dir/train.tsv $save_test_data_dir/$dset.tsv
     cp -r $save_test_data_dir/* $save_l_data_dir
+    cp -r $save_test_data_dir/* $save_ul_data_dir
 done
