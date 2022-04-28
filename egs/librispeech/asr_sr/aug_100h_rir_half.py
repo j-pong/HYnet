a17b8bec0e839ffd081fca46cb7a2dd0da88df9a
JuseokSeong <as2835510@gmail.com>
2022-04-29 00:48:51 +0900

    modify rir_augment_file to identify augmentation

--- egs/librispeech/asr_sr/aug_100h_rir_half.py
+++ egs/librispeech/asr_sr/aug_100h_rir_half.py
@@ -12,10 +12,11 @@ start = time.time()
 math.factorial(100000) 
 
 
-if not os.path.isdir("/DB/LibriSpeech/LibriSpeech/RIR"):
-    print("Download RIR file")
-    print("RIR is in ASML03..")
-    exit()
+# local and server RIR folder is different
+# if not os.path.isdir("/DB/LibriSpeech/LibriSpeech/RIR"):
+#     print("Download RIR file")
+#     print("RIR is in ASML03..")
+#     exit()
     
 # make "aug_ultrain.tsv" in train_aug_ul860 folder
 if not os.path.isdir("./data/fairseq/train_aug100_half"):
@@ -28,7 +29,7 @@ with open("./data/fairseq/train_clean_100/train.tsv" , "r") as f:
     with open("./data/fairseq/train_aug100_half/train_aug.tsv", 'w') as aug_f: 
         aug_f.write(save_root_dir+'\n')
 
-        with open("/DB/LibriSpeech/LibriSpeech/RIR/RIRlist.txt", 'r') as aug:      
+        with open("/DB/Augmentation/RIRlist.txt", 'r') as aug:      
             lines=aug.read().splitlines()
             aug_wavs={} 
 
@@ -48,19 +49,20 @@ with open("./data/fairseq/train_clean_100/train.tsv" , "r") as f:
             if not os.path.isdir(('/').join(path_new.split('/')[:-1])):
                 os.makedirs(('/').join(path_new.split('/')[:-1]))
             
+            # print(path_or_fp)
             wav, curr_sample_rate = sf.read(path_or_fp, dtype="float32")             
             
             if aug == 1:    
                 rir_path=lines[rand_number]
-                y_rir, _ = librosa.load("/DB/LibriSpeech/LibriSpeech/RIR" + rir_path.lstrip('.'), sr=16000)
+                y_rir, _ = librosa.load("/DB/Augmentation/" + rir_path.lstrip('.'), sr=16000)
 
                 aug_wav = np.convolve(wav, y_rir)
             
                 sf.write("/DB/LibriSpeech/LibriSpeech/train-aug100_half/" + items[0][:index] + "aug" +items[0][index:], aug_wav, curr_sample_rate)
-                aug_f.write(items[0][:index] + "aug" +items[0][index:]+"\t"+items[1] +"\n")
+                aug_f.write(items[0][:index] + "aug" +items[0][index:]+"\t"+items[1] + "\t" + "rvb1" +"\n")
             else:
                 sf.write("/DB/LibriSpeech/LibriSpeech/train-aug100_half/" + items[0][:index] +items[0][index:], wav, curr_sample_rate)
-                aug_f.write(items[0][:index] + items[0][index:]+"\t"+items[1] +"\n")
+                aug_f.write(items[0][:index] + items[0][index:]+"\t"+items[1] + "\t" + "rvb0" +"\n")
 
 end = time.time() 
 print(f"{end - start:.5f} sec")		
