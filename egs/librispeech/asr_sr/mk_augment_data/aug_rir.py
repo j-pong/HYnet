d86b6dee8c8f39c7954e19e773566947816753fc
JuseokSeong <as2835510@gmail.com>
2022-06-27 12:13:34 +0900

    modified augment file

--- egs/librispeech/asr_sr/mk_augment_data/aug_rir.py
+++ egs/librispeech/asr_sr/mk_augment_data/aug_rir.py
@@ -8,27 +8,42 @@ import math
 import time 
 import gzip
 
+import argparse
+
 start = time.time() 
 math.factorial(100000) 
 
 
-if not os.path.isdir("/DB/LibriSpeech/LibriSpeech/RIR"):
-    print("Download RIR file")
-    print("RIR is in ASML03.. ^^*")
+def add_args(parser):
+    parser.add_argument("--rir_dir", default="/DB/LibriSpeech/LibriSpeech/RIR")
+    parser.add_argument("--train_860", default="./data/fairseq/train_l100_ul860/ultrain.tsv")
+    parser.add_argument("--train_aug_860", default="./data/fairseq/train_aug_ul860")
+    parser.add_argument("--train_aug_DB", default="/DB/LibriSpeech/LibriSpeech/train-aug860")
+    parser.add_argument("--aug_ratio", type=int, default=100, help="5, 10, 25, 50 100")
+    return parser
+
+parser = argparse.ArgumentParser()
+parser = add_args(parser)
+args = parser.parse_args()
+
+if not os.path.isdir(args.rir_dir):
+    print("rir_dir have to contain rir wav file and rir list(RIRlist.txt)")
+    print("Download or make rir dir")
+    print("exit")
     exit()
     
 # make "aug_ultrain.tsv" in train_aug_ul860 folder
-if not os.path.isdir("./data/fairseq/train_aug_ul860"):
-    os.makedirs("./data/fairseq/train_aug_ul860")
+if not os.path.isdir(args.train_aug_860):
+    os.makedirs(args.train_aug_860)
 
-with open("./data/fairseq/train_l100_ul860/ultrain.tsv" , "r") as f:
+with open(args.train_860 , "r") as f:
     root_dir = f.readline().strip()
-    save_root_dir = "/DB/LibriSpeech/LibriSpeech/train-aug860"
+    save_root_dir = args.train_aug_DB
 
-    with open("./data/fairseq/train_aug_ul860/aug_ultrain.tsv", 'w') as aug_f: 
+    with open(args.train_aug_860 + "/aug_ultrain.tsv", 'w') as aug_f: 
         aug_f.write(save_root_dir+'\n')
 
-        with open("/DB/LibriSpeech/LibriSpeech/RIR/RIRlist.txt", 'r') as aug:      
+        with open(args.rir_dir + "/RIRlist.txt", 'r') as aug:      
             lines=aug.read().splitlines()
             aug_wavs={} 
 
@@ -46,7 +61,7 @@ with open("./data/fairseq/train_l100_ul860/ultrain.tsv" , "r") as f:
             wav, curr_sample_rate = sf.read(path_or_fp, dtype="float32")             
         
             rir_path=lines[rand_number]
-            y_rir, _ = librosa.load("/DB/LibriSpeech/LibriSpeech/RIR" + rir_path.lstrip('.'), sr=16000)
+            y_rir, _ = librosa.load(args.rir_dir + rir_path.lstrip('.'), sr=16000)
 
             aug_wav = np.convolve(wav, y_rir)
             
@@ -55,7 +70,7 @@ with open("./data/fairseq/train_l100_ul860/ultrain.tsv" , "r") as f:
 
             index = items[0].find('flac') -1
 
-            sf.write("/DB/LibriSpeech/LibriSpeech/train-aug860/" + items[0][:index] + "aug" +items[0][index:], aug_wav, curr_sample_rate)
+            sf.write(args.train_aug_DB + "/" + items[0][:index] + "aug" +items[0][index:], aug_wav, curr_sample_rate)
             aug_f.write(items[0][:index] + "aug" +items[0][index:]+"\t"+items[1] +"\n")
 
 
